a94901632a1b98a2dd6d616dd8ec66af9ed3398d
Jonny McFadden <jmcfadden1528@gmail.com>
2019-03-03 18:14:22 -0600

    completed football

--- Football/Program.cs
+++ Football/Program.cs
@@ -9,14 +9,14 @@ namespace Football
             Console.Write("Enter the number of touchdowns(7 points): ");
             int touchdowns = int.Parse(Console.ReadLine());
 
-            Console.Write("Enter the number of field goals(3 points: ");
+            Console.Write("Enter the number of field goals(3 points): ");
             int fieldgoals = int.Parse(Console.ReadLine());
 
             // Compute the number of points in a single line of code.
             int points = 0;
 
+            points = (touchdowns * 7) + (fieldgoals * 3);
             Console.WriteLine($"The team scored {points} points.");
-
             Console.ReadLine();
         }
     }
